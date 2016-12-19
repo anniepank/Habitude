0e825cb70d3a0149a6b09d46752adf37181785b4
Anna Pankova <anya.pankova@gmail.com>
2016-12-19 15:20:31 +0300

    Fix home button

--- app/src/main/java/com/example/android/habittracker/EditHabitActivity.java
+++ app/src/main/java/com/example/android/habittracker/EditHabitActivity.java
@@ -15,7 +15,7 @@ import com.example.android.habittracker.data.Habit;
 import com.example.android.habittracker.data.Settings;
 
 public class EditHabitActivity extends AppCompatActivity {
-
+    EditText editHabitName;
     Habit edit_habit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,14 +30,12 @@ public class EditHabitActivity extends AppCompatActivity {
         CollapsingToolbarLayout collapsingToolbar =
                 (CollapsingToolbarLayout) findViewById(R.id.collapsing);
 
-        final EditText e = (EditText) findViewById(R.id.edit_habit_name);
-        e.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        editHabitName = (EditText) findViewById(R.id.edit_habit_name);
+        editHabitName.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if (actionId == 0 || actionId == 6) {
-                    edit_habit.habitName = e.getText().toString();
-                    EditHabitActivity.this.finish();
-                    Settings.global.save(EditHabitActivity.this);
+                    saveAndFinish();
                 }
                 return false;
             }
@@ -45,7 +43,7 @@ public class EditHabitActivity extends AppCompatActivity {
         Intent intent = getIntent();
         int habit_number = intent.getIntExtra("habit_number", -1);
         edit_habit = Settings.global.habits.get(habit_number);
-        e.setText(edit_habit.habitName);
+        editHabitName.setText(edit_habit.habitName);
 
         collapsingToolbar.setTitle(edit_habit.habitName);
     }
@@ -62,7 +60,20 @@ public class EditHabitActivity extends AppCompatActivity {
             Settings.global.save(this);
             finish();
         }
+        if(item.getItemId() == android.R.id.home) {
+            saveAndFinish();
+        }
         return true;
+    }
+
+    public void saveAndFinish() {
+        edit_habit.habitName = editHabitName.getText().toString();
+        EditHabitActivity.this.finish();
+        Settings.global.save(EditHabitActivity.this);
+    }
+    @Override
+    public void onBackPressed() {
+        saveAndFinish();
     }
 }
 
