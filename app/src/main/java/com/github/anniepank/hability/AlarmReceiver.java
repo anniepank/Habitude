277f83b0219ae991a8705a0a501f58cdf8362ba5
Anna Pankova <anya.pankova@gmail.com>
2017-01-06 17:37:34 +0300

    Reminder opens habit

--- app/src/main/java/com/github/anniepank/hability/AlarmReceiver.java
+++ app/src/main/java/com/github/anniepank/hability/AlarmReceiver.java
@@ -2,11 +2,13 @@ package com.github.anniepank.hability;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import com.github.anniepank.hability.activities.MainActivity;
 import com.github.anniepank.hability.data.Habit;
 import com.github.anniepank.hability.data.Settings;
 
@@ -20,7 +22,8 @@ public class AlarmReceiver extends BroadcastReceiver {
                 .setContentTitle("Hability")
                 .setContentText("It's time for " + habit.habitName)
                 .setDefaults(Notification.DEFAULT_VIBRATE)
-                .setPriority(Notification.PRIORITY_MAX);
+                .setPriority(Notification.PRIORITY_MAX)
+                .setContentIntent(PendingIntent.getActivity(context, 101, new Intent(context, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT));
         Notification notification = builder.build();
 
         Log.i("Notify", intent.getIntExtra("habitNumber", 0) + "");
